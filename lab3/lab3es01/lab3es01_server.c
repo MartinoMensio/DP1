e93b080347dbf360268ccecc5f6cf18fc2efba18
MartinoMensio <martinomensio@outlook.it>
2016-06-16 20:48:59 +0200

    lab3es01 concurrent server

--- lab3/lab3es01/lab3es01_server.c
+++ lab3/lab3es01/lab3es01_server.c
@@ -13,6 +13,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/wait.h>
 
 #define BACKLOG 250
 #define MAX_REQUEST_LEN 512
@@ -23,6 +24,8 @@
 #define QUIT_MSG "QUIT"
 #define OK_MSG "+OK\r\n"
 #define DATA_CHUNK_SIZE 1024*1024
+
+#define MAX_CHILDREN 3
 
 int sigpipe;
 
@@ -46,6 +49,8 @@ int main(int argc, char *argv[]) {
   uint32_t file_size, last_modification, file_size_n, last_modification_n;
   struct stat sb;
   uint n_read;
+  
+  int pid, cpid, nchildren;
   
   
   if(argc != 2) {
@@ -85,133 +90,162 @@ int main(int argc, char *argv[]) {
   
   signal(SIGPIPE, sigpipeHndlr);
   
+  nchildren = 0;
+  
   while(1) {
+    
+    // check if can accept and generate a new process
+    if(nchildren >= MAX_CHILDREN) {
+      // collect a zombie
+      printf("PARENT - Reached maximum number of children. Waiting for one of them\n");
+      wait(NULL);
+      nchildren--;
+      printf("PARENT - A children has been waited\n");
+      continue;
+    }
+    
+    printf("PARENT - Waiting for a client to connect\n");
     caddr_len = sizeof(caddr);
-    
-    printf("Waiting for a client to connect\n");
-    
     s_accepted = accept(s_listen, (struct sockaddr *)&caddr, &caddr_len);
     
-    sigpipe = 0;
-    
     if(s_accepted < 0) {
-      perror("Impossible to accept");
+      perror("PARENT - Impossible to accept");
       break;
     }
+    printf("PARENT - Accepted a connection from %s:%d\n", inet_ntoa(caddr.sin_addr), ntohs(caddr.sin_port));
     
-    printf("Accepted a connection from %s:%d\n", inet_ntoa(caddr.sin_addr), ntohs(caddr.sin_port));
-    
-    fsock_in = fdopen(s_accepted, "r");
-    if(fsock_in == NULL) {
-      perror("Impossible to do fdopen r");
-      close(s_accepted);
-      continue;
+    cpid = fork();
+    if(cpid < 0) {
+      perror("Impossible to fork");
+      return 1;
     }
-    fsock_out = fdopen(s_accepted, "w");
-    if(fsock_out == NULL) {
-      perror("Impossible to do fdopen w");
-      fclose(fsock_in);
-      close(s_accepted);
-      continue;
-    }
-    
-    setbuf(fsock_out, 0);
-    
-    receive_requests = 1;
-    while(receive_requests) {
-      memset(request, 0, MAX_REQUEST_LEN);
-      if(fgets(request, MAX_REQUEST_LEN, fsock_in) == NULL) {
-        perror("Impossible to read a line from the socket. Maybe client closed connection");
-        receive_requests = 0;
-        break;
+    if(cpid) {
+      // parent
+      
+      // one more child is born
+      nchildren++;
+      
+    } else {
+      // child
+      pid = getpid();
+      sigpipe = 0;
+      
+      fsock_in = fdopen(s_accepted, "r");
+      if(fsock_in == NULL) {
+        perror("Impossible to do fdopen r");
+        close(s_accepted);
+        return 1;
+      }
+      fsock_out = fdopen(s_accepted, "w");
+      if(fsock_out == NULL) {
+        perror("Impossible to do fdopen w");
+        fclose(fsock_in);
+        close(s_accepted);
+        return 1;
       }
       
-      printf("Received a request: %s\n", request);
+      setbuf(fsock_out, 0);
       
-      if(sscanf(request, "%s", type) != 1) {
-        // maybe empty line?
-        printf("Type of request not found\n");
-        fprintf(fsock_out, ERR_MSG);
-        continue;
-      }
-      
-      if(strncmp(type, QUIT_MSG, MAX_COMMAND_LEN) == 0) {
-        printf("Received a quit message\n");
-        break;
-      }
-      if(strncmp(type, GET_MSG, MAX_COMMAND_LEN) != 0) {
-        printf("Received an unknown request type\n");
-        fprintf(fsock_out, ERR_MSG);
-        break;
-      }
-      
-      // from now on, it is a get message
-      if(sscanf(request, "%s %s", type, filename) != 2) {
-        printf("Wrong format in GET request\n");
-        fprintf(fsock_out, ERR_MSG);
-        continue;
-      }
-      
-      printf("Parsed request: TYPE=%s FILENAME=%s\n", type, filename);
-      
-      if(stat(filename, &sb)) {
-        perror("Impossible to stat requested file");
-        fprintf(fsock_out, ERR_MSG);
-        continue;
-      }
-      
-      if((sb.st_mode & S_IFMT) != S_IFREG) {
-        perror("The requested file is no a regular file");
-        fprintf(fsock_out, ERR_MSG);
-        continue;
-      }
-      
-      file_size = sb.st_size;
-      last_modification = sb.st_mtim.tv_sec;
-      file_size_n = htonl(file_size);
-      last_modification_n = htonl(last_modification);
-      
-      file = fopen(filename, "r");
-      if(file == NULL) {
-        perror("Impossible to open requested file");
-        fprintf(fsock_out, ERR_MSG);
-        continue;
-      }
-      
-      printf("Sending size and modification time\n");
-      
-      fprintf(fsock_out, "%s", OK_MSG);
-      fwrite(&file_size_n, sizeof(uint32_t), 1, fsock_out);
-      fwrite(&last_modification_n, sizeof(uint32_t), 1, fsock_out);
-      
-      printf("Sending the file\n");
-      
-      while((n_read = fread(buffer, sizeof(char), DATA_CHUNK_SIZE, file)) > 0) {
-        fwrite(buffer, sizeof(char), n_read, fsock_out);
-        if(sigpipe) {
-          printf("Client closed socket\n");
+      receive_requests = 1;
+      while(receive_requests) {
+        memset(request, 0, MAX_REQUEST_LEN);
+        if(fgets(request, MAX_REQUEST_LEN, fsock_in) == NULL) {
+          perror("Impossible to read a line from the socket. Maybe client closed connection");
           receive_requests = 0;
-          fclose(file);
           break;
         }
+        
+        printf("CHILD %d - Received a request: %s\n", pid, request);
+        
+        if(sscanf(request, "%s", type) != 1) {
+          // maybe empty line?
+          printf("CHILD %d - Type of request not found\n", pid);
+          fprintf(fsock_out, ERR_MSG);
+          continue;
+        }
+        
+        if(strncmp(type, QUIT_MSG, MAX_COMMAND_LEN) == 0) {
+          printf("CHILD %d - Received a quit message\n", pid);
+          break;
+        }
+        if(strncmp(type, GET_MSG, MAX_COMMAND_LEN) != 0) {
+          printf("CHILD %d - Received an unknown request type\n", pid);
+          fprintf(fsock_out, ERR_MSG);
+          break;
+        }
+        
+        // from now on, it is a get message
+        if(sscanf(request, "%s %s", type, filename) != 2) {
+          printf("CHILD %d - Wrong format in GET request\n", pid);
+          fprintf(fsock_out, ERR_MSG);
+          continue;
+        }
+        
+        printf("CHILD %d - Parsed request: TYPE=%s FILENAME=%s\n", pid, type, filename);
+        
+        if(stat(filename, &sb)) {
+          perror("Impossible to stat requested file");
+          fprintf(fsock_out, ERR_MSG);
+          continue;
+        }
+        
+        if((sb.st_mode & S_IFMT) != S_IFREG) {
+          perror("The requested file is no a regular file");
+          fprintf(fsock_out, ERR_MSG);
+          continue;
+        }
+        
+        file_size = sb.st_size;
+        last_modification = sb.st_mtim.tv_sec;
+        file_size_n = htonl(file_size);
+        last_modification_n = htonl(last_modification);
+        
+        file = fopen(filename, "r");
+        if(file == NULL) {
+          perror("Impossible to open requested file");
+          fprintf(fsock_out, ERR_MSG);
+          continue;
+        }
+        
+        printf("CHILD %d - Sending size and modification time\n", pid);
+        
+        fprintf(fsock_out, "%s", OK_MSG);
+        fwrite(&file_size_n, sizeof(uint32_t), 1, fsock_out);
+        fwrite(&last_modification_n, sizeof(uint32_t), 1, fsock_out);
+        
+        printf("CHILD %d - Sending the file\n", pid);
+        
+        while((n_read = fread(buffer, sizeof(char), DATA_CHUNK_SIZE, file)) > 0) {
+          fwrite(buffer, sizeof(char), n_read, fsock_out);
+          if(sigpipe) {
+            printf("CHILD %d - Client closed socket\n", pid);
+            receive_requests = 0;
+            fclose(file);
+            break;
+          }
+        }
+        if(!receive_requests) {
+          break;
+        }
+        fclose(file);
+        
+        printf("CHILD %d - File sent\n", pid);
       }
-      if(!receive_requests) {
-        break;
-      }
-      fclose(file);
       
-      printf("File sent\n");
+      fclose(fsock_out);
+      fclose(fsock_in);
+      close(s_accepted);
+      
+      printf("CHILD %d - Terminating\n", pid);
+      return 0;
     }
     
-    fclose(fsock_out);
-    fclose(fsock_in);
-    close(s_accepted);
-    
   }
+  return 0; // useless
 }
 
 void sigpipeHndlr(int signal) {
-  printf("SIGPIPE captured!\n");
+  printf("PROCESS %d - SIGPIPE captured!\n", getpid());
   sigpipe = 1;
   return;
 }
